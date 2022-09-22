e8d879a37196a22a5f9fe340357f64327e9f9151
George Hatzigeorgio <georgehatzigeorgio@zohomail.com>
2022-09-22 06:29:59 -0400

    css for card done need to do breakpoints

--- ketodogapp/src/Components/SignUp.js
+++ ketodogapp/src/Components/SignUp.js
@@ -47,6 +47,10 @@ function SignUp() {
         console.log(err);
       });
   };
+  useEffect(() => {
+    postNewSignUp();
+  }, []);
+
   const inputChange = (name, value, evt) => {
     yup
       .reach(SignUpSchema, name)
@@ -104,7 +108,8 @@ function SignUp() {
         <br />
         <br />
         <h1 className="title">
-          Welcome to Ketogenic Diets For Dog Cancer! Please Sign Up To Access Important Resources!
+          Welcome to Ketogenic Diets For Dog Cancer! Please Sign Up To Access
+          Important Resources!
         </h1>
         <form onSubmit={onSubmit} className="signup-form">
           <div>
