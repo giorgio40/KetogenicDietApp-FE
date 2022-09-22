2c0513bad2006d3a717e9b9bef130459f7e49477
George Hatzigeorgio <georgehatzigeorgio@zohomail.com>
2022-09-22 08:16:09 -0400

    footer done

--- ketodogapp/src/Components/Footer.js
+++ ketodogapp/src/Components/Footer.js
@@ -5,27 +5,27 @@ function Footer() {
     <section className="footer-section">
       <section className="footer-right">
         <div className="links">Important Links</div>
-        <div>
+        <div className="link-containerg">
           <a href="http://vhavets.com">
             Veterinary HealthCare Associates
           </a>
         </div>
-        <div>
+        <div className="link-container">
           <a href="http://longlivingpets.com">
             LongLivingPets
           </a>
         </div>
-        <div>
+        <div className="link-container">
           <a href="http://ketopoweredk9.com">
             KetoPoweredK9
           </a>
         </div>
-        <div>
+        <div className="link-container">
           <a href="https://www.amazon.com/Cancer-Metabolic-Disease-Management-Prevention/dp/0470584920">
             Cancer as a Metabolic Disease Book by Dr. Thomas Seyfried
           </a>
         </div>
-        <div>
+        <div className="link-container">
           <a href="https://www.amazon.com/Tripping-over-Truth-Overturning-Entrenched-ebook/dp/B01N25FPY9/ref=sr_1_1?keywords=tripping+over+the+truth&qid=1639597230&s=books&sr=1-1">
             Tripping Over the Truth book by Travis Cristopherson
           </a>
